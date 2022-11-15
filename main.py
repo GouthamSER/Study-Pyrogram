c3b562ebbf2be413d6e994c92a70ef6ff558f08d
i.am.goutham <97418751+GouthamSER@users.noreply.github.com>
2022-11-15 19:40:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
-API_ID = ""
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = "18979569"
+API_HASH = "45db354387b8122bdf6c1b0beef93743"
+BOT_TOKEN = "5530146236:AAGs0pJc11GSk6uxFZ3eYuVcP2DOXMWTlB4"
 
 GOUTHAM SER = Client(
      name="Pyrogram Bot",
