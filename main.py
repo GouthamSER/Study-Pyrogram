b593abe6baa200e7ed6d6e449b59b6ef2f0d47c8
i.am.goutham <97418751+GouthamSER@users.noreply.github.com>
2022-11-15 19:43:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ API_ID = "18979569"
 API_HASH = "45db354387b8122bdf6c1b0beef93743"
 BOT_TOKEN = "5530146236:AAGs0pJc11GSk6uxFZ3eYuVcP2DOXMWTlB4"
 
-GOUTHAM SER = Client(
+GOUTHAM = Client(
      name="Pyrogram Bot",
      api_id=API_ID,
      api_hash=API_HASH,
@@ -13,4 +13,4 @@ GOUTHAM SER = Client(
 
 print("BOT STARTED")
 
-GOUTHAM SER.run()
+GOUTHAM.run()
