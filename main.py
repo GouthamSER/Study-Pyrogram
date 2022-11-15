f191d24a424662fd79ffebf2aab85bf718d732dd
i.am.goutham <97418751+GouthamSER@users.noreply.github.com>
2022-11-15 19:37:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from pyrogram import Client, filters
 
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+GOUTHAM SER = Client(
+     name="Pyrogram Bot",
+     api_id=API_ID,
+     api_hash=API_HASH,
+     bot_token=BOT_TOKEN
+)
+
+print("BOT STARTED")
+
+GOUTHAM SER.run()
