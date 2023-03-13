784f60000f3fb36eee6d60272a4710b62f7c49df
i.am.goutham <97418751+GouthamSER@users.noreply.github.com>
2023-03-13 20:42:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ API_ID = "18979569"
 API_HASH = "45db354387b8122bdf6c1b0beef93743"
 BOT_TOKEN = "5816309562:AAHPAhIv-oRv1YxEL0X9a3MFQEYfuCw8wbk"
 
-@Client = Client(
+@Goutham = Client(
      name="Pyrogram Bot",
      api_id=API_ID,
      api_hash=API_HASH,
@@ -28,7 +28,7 @@ BOT_TOKEN = "5816309562:AAHPAhIv-oRv1YxEL0X9a3MFQEYfuCw8wbk"
         PORT_CODE).start()
 
 
-@Client.on_message(filters.command("start"))
+@Goutham.on_message(filters.command("start"))
 async def start_cmd(client, message):
      await message.reply_text(
           text="HELLO I AM PYROGRAM BOT\t DEVELPOING.......",
@@ -43,20 +43,20 @@ async def start_cmd(client, message):
           )
      )
      
-@Client.on_message(filters.regex("START🥰"))
+@Goutham.on_message(filters.regex("START🥰"))
 async def start_keyboard(client, message):
      await message.reply_text(
           text="HI IAM PYROGRAM STUDING BOT"
      )
 
-@Client.on_message(filters.regex("HELP🙄"))
+@Gouthamt.on_message(filters.regex("HELP🙄"))
 async def help_keyboard(client, message):
      await message.reply_text(
           text="NOTHING🤣🤣🤣🤣🤣🤣 IN THE HELP"
      )    
      
      
-@Client.on_message(filters.regex("ABOUT😁"))
+@Goutham.on_message(filters.regex("ABOUT😁"))
 async def about_keyboard(client, message):
      await message.reply_text(
           text="ABOUT \nMADE WITH PYTHON LANGUAGE\nServer:Heroku\n\nTHIS BOT IS MADE BY GOUTHAM SER"
@@ -66,7 +66,7 @@ async def about_keyboard(client, message):
 
 #button inline 
      
-@Client.on_message(filters.command("start"))
+@Goutham.on_message(filters.command("start"))
 async def start_cmd(client, message):
      button = [[
           InlineKeyboardButton("OWNER",  url="t.me/im_goutham_josh")
@@ -81,7 +81,7 @@ async def start_cmd(client, message):
      
      
  # callback query 
-@Client.on_callback_query()
+@Goutham.on_callback_query()
 async def Goutham_cb(client: GOUTHAM, query: CallbackQuery):
      if query.data == "start":
           await query.message.edit(
@@ -96,4 +96,4 @@ print("
 ░█─▄▄ ░█──░█ ░█─░█ ─░█── ░█▀▀█ ░█▄▄█ ░█░█░█ 　 ─▀▀▀▄▄ 　 ░█▀▀▀ 　 ░█▄▄▀ 
 ░█▄▄█ ░█▄▄▄█ ─▀▄▄▀ ─░█── ░█─░█ ░█─░█ ░█──░█ 　 ░█▄▄▄█ 　 ░█▄▄▄ 　 ░█─░█")
 
-@Client.run()
+@Goutham.run()
